310cfaea4bdff1f46671bc6fff56f38d9a4a3b20
Sergey Mazurkin <mazzy@mazzy.ru>
2018-05-16 13:49:56 +0300

    appveyor

--- examples/parallel-split.ps1
+++ examples/parallel-split.ps1
@@ -1,6 +1,4 @@
-#Requires -Version 5
 #Requires -Module xpoTools
-#Requires -Module SplitPipeline
 
 # To install modules from https://www.powershellgallery.com/packages/xpoTools run powershell commands:
 # PS> Install-Module xpoTools
@@ -8,21 +6,23 @@
 #
 # see https://github.com/mazzy-ax/xpoTools and https://github.com/nightroman/SplitPipeline for details
 
-Set-StrictMode -Version Latest
+if ( get-module SplitPipeline -ListAvailable ) {
+    $load = 0 # 0 is auto. Try 10, 50, 100 or another values for a big xpo file.
 
-$load = 0 # 0 is auto. Try 10, 50, 100 or another values for a big xpo file.
+    $projectRoot = (Resolve-Path $PSScriptRoot\..).Path
+    $data = @{
+        projectRoot = $projectRoot
+    }
 
-$projectRoot = (Resolve-Path $PSScriptRoot\..).Path
-$data = @{
-    projectRoot = $projectRoot
-}
+    Write-Verbose 'parallel split'
+    Get-ChildItem $projectRoot\tests\assets\*.xpo |
+        Import-Xpo |
+        Split-Pipeline -Verbose -Load $load -Variable data -Module xpoTools {process {
 
-Write-Verbose 'parallel split'
-Get-ChildItem $projectRoot\tests\assets\*.xpo |
-    Import-Xpo |
-    Split-Pipeline -Verbose -Load $load -Variable data -Module xpoTools {process {
-
-        Split-xpo -Items $_ -Destination $data.projectRoot, \.test -xpp -PathStyle mazzy -Encoding UTF8 -PassThru
+            Split-xpo -Items $_ -Destination $data.projectRoot, \.test -xpp -PathStyle mazzy -Encoding UTF8 -PassThru
+        }
     }
 }
-
+else {
+    Write-Warning "Module SplitPipeline is not installed."
+}
